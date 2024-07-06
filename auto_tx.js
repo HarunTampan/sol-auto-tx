0a7e584455a1ab2018c8af2af8135912a67767fd
HarunTampan <97161524+HarunTampan@users.noreply.github.com>
2024-07-06 13:51:10 +0700

    Update auto_tx.js

--- auto_tx.js
+++ auto_tx.js
@@ -86,7 +86,7 @@ const {
     const randomAddresses = generateRandomAddresses(keypairs.length * 100);
     console.log(`Generated ${keypairs.length * 100} random addresses:`, randomAddresses);
   
-    const amountToSend = 0.001; // minimum 0.001 SOL
+    const amountToSend = 0.0001; // minimum 0.001 SOL
     let currentKeypairIndex = 0;
     const delayBetweenRequests = 5000; // replace if the network is congested
   
